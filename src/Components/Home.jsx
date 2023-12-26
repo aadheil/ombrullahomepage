69f7fa7e91bd1359cd001de6808dc2ded6a98792
aadheil <adilkkinbox@gmail.com>
2023-12-26 20:32:43 +0530

    Responsive menubar completed

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Menubar from './Menubar'
+import './menubar.css'
 function Home() {
   return (
     <div>
         <Menubar/>
-        
+        <div className='containerr w-full' style={{height:'100vh'}}>
+            hajnjkl Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam illo ex, qui fugit aut doloribus quibusdam ab! Natus commodi assumenda quae dolore, pariatur sed necessitatibus eaque, deserunt, in repudiandae tempora?
+        </div>
     </div>
   )
 }
--- src/Components/Menubar.jsx
+++ src/Components/Menubar.jsx
@@ -1,13 +1,26 @@
 import React, { useState } from 'react'
+import './menubar.css'
 function Menubar() {
     const [isMouseHoverServices, setisMouseHoverServices] = useState(false);
     const [isMouseHoverProducts, setisMouseHoverProducts] = useState(false);
+    const [isSmallScreen,setisSmallScreen]=useState(false)
+   
+    const handlesmalscreenbutton=(e)=>{
+        e.preventDefault()
+        setisSmallScreen(true)
+    }
+
+    const handlesmalscreenbuttonClose=(e)=>{
+        e.preventDefault()
+        setisSmallScreen(false)
+    }
+
 
   return (
     <>
     <div className='flex justify-between'>
-            <div className='ms-20 '><img src="https://www.ombrulla.com/logo.svg" alt="" style={{width:'140px',height:'60px'}} /></div>
-            <div className='flex mt-6 '>
+            <div className='lg:ms-20 '><img src="https://www.ombrulla.com/logo.svg" alt="" style={{width:'140px',height:'60px'}} /></div>
+            <div className='menuItems flex mt-6'>
                 <div className='font-bold hover:text-blue-700'>Home</div>
                 
                 <div className='services ms-8 font-bold  hover:text-blue-700'
@@ -23,10 +36,17 @@ function Menubar() {
                 <div className='ms-8 font-bold hover:text-blue-700'>Blog</div>
                 <div className='ms-8 me-32 font-bold hover:text-blue-700'>Contact</div>
             </div>
+            <div className='icon-for-small-screens flex mt-5 me-5'>
+               { !isSmallScreen?<button onClick={e=>handlesmalscreenbutton(e)}><i className="fa-solid fa-bars fa-2x text-blue-700"></i></button>
+               :
+               <button onClick={e=>handlesmalscreenbuttonClose(e)}><i className="fa-solid fa-xmark fa-2x text-blue-700"></i></button>
+               }
+            </div>
         </div>
 
-        {isMouseHoverServices&&<div className="flex justify-end p-4 me-96">
-        <div className="bg-white shadow text-black p-2 flex flex-col">
+    {/* content to be display on hovering on service , products */}
+        {isMouseHoverServices&&<div className=" flex justify-end p-4 me-96">
+        <div className="modalcontento bg-white shadow text-black p-2 flex flex-col">
        <p className='mt-2 mb-2 font-bold'> AI Visual Inspection</p>
        <hr />
        <p className='mt-2 mb-2 font-bold'> AI InfraStructure Inspection </p>
@@ -37,14 +57,18 @@ function Menubar() {
         </div>
       </div>}
 
-      {isMouseHoverProducts&&<div className="flex justify-end p-4 me-80">
-        <div className="bg-white shadow text-black p-2 flex flex-col">
+      {isMouseHoverProducts&&<div className=" flex justify-end p-4 me-80">
+        <div className="modalcontente bg-white shadow text-black p-2 flex flex-col">
        <p className='mt-2 mb-2 font-bold'> Asset Performance Management</p>
        
        
         </div>
       </div>}
 
+{/* Menubar for small screens */}
+    {isSmallScreen&&<div className='smalScreenMenubar bg-white shadow' style={{width:'75%',height:'100%',zIndex:'10'}}>
+        <div><h1>hello</h1></div>
+    </div>}
 
     </>
   )
